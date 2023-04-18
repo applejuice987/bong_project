08afb19c1f3cdb7864620cf3c853c1e0f41f29db
minsu <wjdalstn8443@naver.com>
2023-04-18 21:44:51 +0900

    api 연결 진행중

--- source/main.c
+++ source/main.c
@@ -3,10 +3,22 @@
 #include "../header/db.h"
 #include "../header/pcapcap.h"
 #include "../header/pcapmodu.h"
+#include <stdio.h>
+#include <curl/curl.h>
 
 int main()
 {
 
-   //minsu_6
+    CURL *hnd = curl_easy_init(); 
+    
+ 
 
+    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(hnd, CURLOPT_URL, "https://www.virustotal.com/api/v3/ip_addresses/ip");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "accept: application/json");
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+    CURLcode ret = curl_easy_perform(hnd);
 }
