b089c085165b56cd98ad8a64f9d6b26139939500
minsu <wjdalstn8443@naver.com>
2023-04-22 03:29:04 +0900

    api 연결확인

--- source/main.c
+++ source/main.c
@@ -3,30 +3,37 @@
 #include "../header/db.h"
 #include "../header/pcapcap.h"
 #include "../header/pcapmodu.h"
+
+
+#include "../header/include/curl/curl.h" 
+#include "../header/include/json/json.h"
+//#include <curl/curl.h>
+//#include <json-c/json.h>
+
 #include <stdio.h>
-#include <curl/curl.h>
 int main()
 {
 
     CURL *hnd = curl_easy_init();
 
+
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
     curl_easy_setopt(hnd, CURLOPT_URL, "https://www.virustotal.com/api/v3/ip_addresses/177.154.84.34");
 
     struct curl_slist *headers = NULL;
-
-    headers = curl_slist_append(headers, "accept:application/json");
+    headers = curl_slist_append(headers, "accept: application/json");
+    headers = curl_slist_append(headers, "x-apikey: c58bade78504ce953865e8cfc05ec4b11cdc144a8f882afd0beb0b20885f9df7");
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
 
     
 
-    while (headers != NULL)
-    {
-        printf("%s\n", headers->data);
-        headers = headers->next;
-    }
-    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+    
+
+
 
     CURLcode ret = curl_easy_perform(hnd);
+
+    printf("%d",ret);
 }
 
 //c58bade78504ce953865e8cfc05ec4b11cdc144a8f882afd0beb0b20885f9df7
